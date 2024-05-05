c91dd456c1d05d1de2cf3b891d961c6fa10623f2
Eduard Gashi <165167290+eduardgashi24@users.noreply.github.com>
2024-05-05 17:05:13 +0200

    Hier werden die Songtexte abgelegt

---
+++ song_lyrics.py
@@ -0,0 +1,168 @@
+in_the_end = """it starts with one
+
+One thing, I don't know why
+It doesn't even matter how hard you try
+Keep that in mind, I designed this rhyme
+To explain in due time, all I know
+
+Time is a valuable thing
+Watch it fly by as the pendulum swings
+Watch it count down to the end of the day
+The clock ticks life away, it's so unreal
+
+Didn't look out below
+Watch the time go right out the window
+Tryin' to hold on, d-didn't even know
+I wasted it all just to watch you go
+
+I kept everything inside
+And even though I tried, it all fell apart
+What it meant to me will eventually be
+A memory of a time when I tried so hard
+
+I tried so hard and got so far
+But in the end, it doesn't even matter
+I had to fall to lose it all
+But in the end, it doesn't even matter
+
+One thing, I don't know why
+It doesn't even matter how hard you try
+Keep that in mind, I designed this rhyme
+To remind myself how I tried so hard
+
+In spite of the way you were mockin' me
+Actin' like I was part of your property
+Remembering all the times you fought with me
+I'm surprised it got so far
+
+Things aren't the way they were before
+You wouldn't even recognize me anymore
+Not that you knew me back then
+But it all comes back to me in the end
+
+You kept everything inside
+And even though I tried, it all fell apart
+What it meant to me will eventually be
+A memory of a time when I tried so hard
+
+I've put my trust in you
+Pushed as far as I can go
+For all this, there's only one thing you should know
+
+I've put my trust in you
+Pushed as far as I can go
+For all this, there's only one thing you should know
+
+I tried so hard and got so far
+But in the end, it doesn't even matter
+I had to fall to lose it all
+But in the end, it doesn't even matter"""
+
+shape_of_you = """A club isn't the best place to find a lover
+So the bar is where I go (Mm)
+Me and my friends at the table doin' shots
+Drinkin' fast and then we talk slow (Mm)
+
+And you come over and start up a conversation with just me
+And trust me, I'll give it a chance now (Mm)
+Take my hand, stop, put Van the Man on the jukebox
+And then we start to dance and now I'm singin' like
+
+Girl, you know I want your love
+Your love was handmade for somebody like me
+Well, come on now, follow my lead
+I may be crazy, don't mind me
+
+Say, "Boy, let's not talk too much"
+Grab on my waist and put that body on me
+Well, come on now, follow my lead
+Come, come on now, follow my lead, mm
+
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is fallin' too
+I'm in love with your body
+
+And last night, you were in my room
+And now my bed sheets smell like you
+Every day, discoverin' somethin' brand new
+Well, I'm in love with your body
+
+
+Oh, I, oh, I, oh, I, oh, I
+Well, I'm in love with your body
+Oh, I, oh, I, oh, I, oh, I
+Well, I'm in love with your body
+
+Oh, I, oh, I, oh, I, oh, I
+Well, I'm in love with your body
+Every day, discoverin' somethin' brand new
+I'm in love with the shape of you
+
+One week in, we let the story begin
+We're goin' out on our first date (Mm)
+You and me are thrifty, so go all-you-can-eat
+Fill up your bag and I fill up a plate (Mm)
+
+We talk for hours and hours about the sweet and the sour
+And how your family is doin' okay (Mm)
+And leave and get in a taxi, and kiss in the back seat
+Tell the driver make the radio play and I'm singin' like
+
+Girl, you know I want your love
+Your love was handmade for somebody like me
+Well, come on now, follow my lead
+I may be crazy, don't mind me
+
+Say, "Boy, let's not talk too much"
+Grab on my waist and put that body on me
+Well, come on now, follow my lead
+Come, come on now, follow my lead, mm
+
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is fallin' too
+I'm in love with your body
+
+And last night, you were in my room
+And now my bed sheets smell like you
+Every day, discoverin' somethin' brand new
+Well, I'm in love with your body
+
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is fallin' too
+I'm in love with your body
+
+And last night, you were in my room
+And now my bed sheets smell like you
+Every day, discoverin' somethin' brand new
+Well, I'm in love with your body
+
+Come on, be my baby, come on
+Come on, be my baby, come on
+I'm in love with your body
+
+Come on, be my baby, come on
+Come on, be my baby, come on
+I'm in love with your body
+
+Come on, be my baby, come on
+Come on, be my baby, come on
+I'm in love with your body
+
+Every day, discoverin' somethin' brand new
+I'm in love with the shape of you"""
+
+song_lyrics = {"Linkin Park - In the End": in_the_end,
+               "Ed Sheeran - Shape of you": shape_of_you}
