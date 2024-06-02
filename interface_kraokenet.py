4c4517a8b774c50415be78be1ef1070e2848698a
Eduard Gashi <165167290+eduardgashi24@users.noreply.github.com>
2024-06-02 11:45:15 +0200

    Update interface_kraokenet.py

--- interface_kraokenet.py
+++ interface_kraokenet.py
@@ -6,21 +6,28 @@ import wave
 import pygame
 import pyaudio
 import time
-from mfcc_comparison import compare_mfcc
+from Vergleich import compare_mfcc
+from speech_comparison import compare_speech
 
-# Pfad, in dem die Karaoke-Version des Benutzers gespeichert wird
-vocals_user_path = r"C:/Users/20edu/Softwareprojekt/pythonProject1/vocals_user.wav"
+
+# Ordner, in dem die Karaoke-Version des Benutzers gespeichert wird
+vocals_user_dir = r"C:/Users/20edu/Softwareprojekt/pythonProject1/vocals_user/"
 song_names = list(songs.keys())  # Liste, in der die Namen aller Lieder sind
-
 
 # Globale Variablen, die wichtig sind, wenn das Video abgebrochen wird (ungültige Karaoke-Version)
 paused = False
 aborted_recording = False
+finished_recording = False
 
 
 def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, format=pyaudio.paInt16, channels=1,
                  rate=44100):
     global paused
+    global finished_recording
+    finished_recording = False
+    global aborted_recording
+    aborted_recording = False  # reset auf False (wenn Aufnahme mehrmals neu gestartet wird)
+
     p = pyaudio.PyAudio()
     stream = p.open(format=format,
                     frames_per_buffer=frames_per_buffer,
@@ -28,9 +35,6 @@ def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, fo
                     rate=rate,
                     input=True)
     frames = []
-
-    global aborted_recording
-    aborted_recording = False  # reset auf False (wenn Aufnahme mehrmals neu gestartet wird)
 
     for i in range(0, int(rate / frames_per_buffer * length_in_seconds)):
         data = stream.read(frames_per_buffer)  # 1024 frames werden in einer Iteration gelesen
@@ -45,12 +49,13 @@ def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, fo
     p.terminate()
 
     if not aborted_recording:  # Aufnahme nur dann speichern, wenn die Karaoke-Version vollständig ist
-        obj = wave.open(output_file_name, "wb")  # wb = write binary
+        obj = wave.open(vocals_user_dir + output_file_name, "wb")  # wb = write binary
         obj.setnchannels(channels)
         obj.setsampwidth(p.get_sample_size(format))
         obj.setframerate(rate)
         obj.writeframes(b''.join(frames))
         obj.close()
+        finished_recording = True
 
 
 def play_audio(audio_file_path):
@@ -88,7 +93,7 @@ def start(song_chosen):
 
     paused = False  # reset auf False
     # Erstellen von Threads, damit gleichzeitig die Karaoke- aufgenommen und die Instrumental-Version abgespielt werden
-    record_thread = threading.Thread(target=record_audio, args=(songs[song_chosen].length, (song_chosen + ".wav")))
+    record_thread = threading.Thread(target=record_audio, args=(songs[song_chosen].length, ("[Karaoke] " + song_chosen + ".wav")))
     play_thread = threading.Thread(target=play_audio, args=(songs[song_chosen].instrumental_path,))
 
     record_thread.start()
@@ -105,10 +110,20 @@ def pause():
     paused = True
 
 
-def estimate_result(song_name):
-    result = compare_mfcc(vocals_user_path, songs[song_name].song_path)  # Hier wird die Vergleichsfunktion aufgerufen
-    return {recording_animation_video: gr.PlayableVideo.update(visible=False),
-            get_result_btn: gr.Button.update(visible=False), result_out: gr.Textbox.update(value=result, visible=True)}
+def estimate_result(song_chosen):
+    global finished_recording
+    if not finished_recording:
+        return {error_txt: gr.Textbox.update(value="Du darfst nicht vorzeitig abbrechen.", visible=True)}
+    karaoke_vocals = vocals_user_dir + "[Karaoke] " + song_chosen + ".wav"
+    result_singing = compare_mfcc(karaoke_vocals, songs[song_chosen].song_path)  # Hier wird die Vergleichsfunktion aufgerufen
+    result_speech = compare_speech(songs[song_chosen], karaoke_vocals)
+
+    return {singing_layout: gr.Column.update(visible=False),
+            result_singing_out: gr.Textbox.update(value=result_singing, visible=True),
+            result_speech_out: gr.Textbox.update(value=result_speech, visible=True),
+            result_legend: gr.Textbox.update(visible=True),
+            error_txt: gr.Textbox.update(visible=False)
+            }
 
 
 with gr.Blocks() as demo:
@@ -133,9 +148,14 @@ with gr.Blocks() as demo:
     with gr.Column() as singing_layout:
         recording_animation_video = gr.PlayableVideo(visible=False, interactive=True)
         get_result_btn = gr.Button(value="Klicke mich um das Ergebnis auszuwerten", visible=False)
-    
-    with gr.Column() as result_layout:
-        result_out = components.Textbox(label="Ergebnis", visible=False)
+
+    with gr.Row() as result_layout:
+        with gr.Column():
+            result_singing_out = components.Textbox(label="Ergebnis Gesang", visible=False)
+            result_speech_out = components.Textbox(label="Ergebnis Sprache", visible=False)
+        result_legend = components.Textbox(label="Legende", value="Die Bewertung erfolgt auf einer Skala von Noten, "
+                                                                  "wobei 1 die Beste und 4 die schlechteste Note ist.",
+                                           visible=False)
 
     train_song_btn.click(fn=get_instrumental_get_lyrics, inputs=[song_chosen_inp], outputs=[instrumental_out,
                                                                                             lyrics_out, error_txt])
@@ -146,8 +166,10 @@ with gr.Blocks() as demo:
     recording_animation_video.play(fn=start, inputs=[song_chosen_inp], outputs=[])
     recording_animation_video.pause(fn=pause, inputs=[], outputs=[])
 
-    get_result_btn.click(fn=estimate_result, inputs=[song_chosen_inp], outputs=[recording_animation_video,
-                                                                                get_result_btn, result_out])
+    get_result_btn.click(fn=estimate_result, inputs=[song_chosen_inp], outputs=[singing_layout,
+                                                                                result_singing_out,
+                                                                                result_speech_out, result_legend,
+                                                                                error_txt])
 
 if __name__ == "__main__":
     demo.launch()
