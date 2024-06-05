c4799d62f61e12d27fd3d020232233fa0e310be3
Jannik Seeburger <jannik.seeburger@continental-corporation.com>
2024-06-05 13:40:56 +0200

    modified Sender.py to also handle receive request for server´. Added the data transfer to server from the interface (commented out)

--- Sender.py
+++ Sender.py
@@ -1,8 +1,13 @@
 import socket
 import pickle
-import wave
-import compareClass
+from . import compareClass
+from . import speech_comparison
+from . import mfcc_comparison
 
+
+def main():
+    while(True):
+        receive()
 
 def send(compare_object: compareClass):
     url = "192.168.43.151"
@@ -11,15 +16,31 @@ def send(compare_object: compareClass):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((url, port))
-            serialized_karaoke = pickle.dumps(karaoke_wav)
+            serialized_karaoke = pickle.dumps(compare_object)
             s.sendall(serialized_karaoke)
             print("wav File was send successfully.")
     except(socket.error, pickle.PickleError) as e:
         print(f"Error: {e}")
 
-def receive(compare_object: compareClass):
+def receive(): 
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(("127.0.0.1", 1337))
+        server_socket.listen(1)
+        print("Waiting for connection...")
 
+        while True: 
+            client_socket, _ = server_socket.accept()
+            with client_socket:
+                in_data = client_socket.recv(1024)
+                received_karaoke = pickle.loads(in_data)
+                received_karaoke.percentage_mfcc = mfcc_comparison.compare_mfcc(received_karaoke.karaoke_wav, received_karaoke.compared_song)
+                recognized_lyrics = speech_comparison.transcribe(received_karaoke.karaoke_wav)
+                received_karaoke.percentage_speech = speech_comparison.compare_speech(received_karaoke.compared_song, recognized_lyrics)
+                send(received_karaoke)
 
+    except (socket.error, pickle.PickleError) as e:
+        raise RuntimeError(e)
 
 if __name__ == "__main__":
     main()
--- interface_kraokenet.py
+++ interface_kraokenet.py
@@ -3,6 +3,8 @@ from gradio import components
 from song import songs
 import wave
 import pyaudio
+from . import mfcc_comparison
+from . import speech_comparison
 from mfcc_comparison import compare_mfcc
 from speech_comparison import compare_speech
 import pandas as pd
@@ -10,6 +12,8 @@ from speech_comparison import transcribe
 from pitch_comparison_transposition import compare_avg_notes
 import Sender
 import compareClass
+import socket
+import pickle
 
 
 
@@ -22,6 +26,21 @@ song_names = list(songs.keys())  # Liste, in der die Namen aller Lieder sind
 paused = False
 aborted_recording = False
 finished_recording = False
+
+def receive(): 
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(("127.0.0.1", 1337))
+        server_socket.listen(1)
+        print("Waiting for connection...")
+
+        while True: 
+            client_socket, _ = server_socket.accept()
+            with client_socket:
+                in_data = client_socket.recv(1024)
+                received_karaoke = pickle.loads(in_data)
+    except (socket.error, pickle.PickleError) as e:
+        raise RuntimeError(e)
 
 
 def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, format=pyaudio.paInt16, channels=1,
