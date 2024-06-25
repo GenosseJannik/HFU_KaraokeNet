f3f2530ea78227229c21f2997773acb636e34099
Eduard Gashi <165167290+eduardgashi24@users.noreply.github.com>
2024-06-25 22:54:50 +0200

    Update interface_kraokenet.py

--- interface_kraokenet.py
+++ interface_kraokenet.py
@@ -85,19 +85,20 @@ def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, fo
 
 def train_song(song_chosen):
     if song_chosen is None:
-        return {error_txt: gr.Textbox(visible=True)}
+        return {error_txt: components.Textbox(visible=True)}
     song_audio = components.Audio(value=songs[song_chosen].song_path, visible=True)
-    instrumental_audio = gr.Audio(value=songs[song_chosen].instrumental_path, visible=True)
+    instrumental_audio = components.Audio(value=songs[song_chosen].instrumental_path, visible=True)
     lyrics = components.Textbox(label=song_chosen, value=songs[song_chosen].lyrics, visible=True)
-    return {song_out: song_audio, instrumental_out: instrumental_audio, lyrics_out: lyrics,
-            error_txt: gr.Textbox(visible=False)}
+    return {testing_layout: gr.Column(visible=True), song_out: song_audio, instrumental_out: instrumental_audio,
+            lyrics_out: lyrics, error_txt: gr.Textbox(visible=False)}
 
 
 def change_layout(song_chosen):
     if song_chosen is None:
-        return {error_txt: gr.Textbox(visible=True)}
+        return {error_txt: components.Textbox(visible=True)}
     animation_video = components.Video(value=songs[song_chosen].animation_path, visible=True)
-    return {starting_layout: gr.Row.update(visible=False), testing_layout: gr.Column(visible=False),
+    return {singing_layout: gr.Column(visible=True),
+            starting_layout: gr.Row.update(visible=False), testing_layout: gr.Column(visible=False),
             recording_animation_video: animation_video,
             get_result_btn: components.Button(visible=True), error_txt: components.Textbox(visible=False)}
 
@@ -114,41 +115,94 @@ def pause():
     paused = True
 
 
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+
+def plot_results(result_singing, result_speech):
+    labels = ["Gesang", "Aussprache"]
+    values = [result_singing, result_speech]
+
+    fig, ax = plt.subplots(figsize=(13, 3), dpi=300)
+
+    bars_color_hex = rgb_to_hex((37, 150, 190))
+    ax.barh(labels, values, color=bars_color_hex)
+    ax.set_xlim(0, 100)
+    ax.set_xticks([0, 50, 100])  # nur bei dem Wert 0, 50 und 100 werden die labels angezeigt
+
+    background_color_hex = rgb_to_hex((34, 41, 54))
+    ax.set_xticklabels(["Ausbaufähig", "Befriedigend", "Sehr gut"])
+    ax.set_facecolor(background_color_hex)
+    fig.patch.set_facecolor(background_color_hex)
+    ax.tick_params(axis='x', colors="white")
+    ax.tick_params(axis='y', colors="white")
+    for spine in ax.spines.values():
+        spine.set_edgecolor("white")
+
+    return fig
+
+
 def estimate_result(song_chosen):
     global finished_recording
     if not finished_recording:
-        return {error_txt: gr.Textbox(value="Du darfst nicht vorzeitig abbrechen.", visible=True)}
+        return {error_txt: components.Textbox(value="Du darfst nicht vorzeitig abbrechen.", visible=True)}
     karaoke_vocals = vocals_user_dir + song_chosen + ".wav"
-    #save wav file as object to transfer to server
-    #karaoke_wav = wave.open(karaoke_vocals)
-    #song_wav = wave.open(songs[song_chosen].song_path)
-    #comparisonObject = compareClass.compareClass(karaoke_wav, song_wav)
-    #Sender.send(comparisonObject)
-    #karaoke_wav.close()
-    #song_wav.close()
-    result_singing = compare_mfcc(karaoke_vocals, songs[song_chosen].song_path)
+    # save wav file as object to transfer to server
+    # karaoke_wav = wave.open(karaoke_vocals)
+    # song_wav = wave.open(songs[song_chosen].song_path)
+    # comparisonObject = compareClass.compareClass(karaoke_wav, song_wav)
+    # Sender.send(comparisonObject)
+    # karaoke_wav.close()
+    # song_wav.close()
+    result_singing_percentage = compare_mfcc(karaoke_vocals, songs[song_chosen].song_path)
     recognized_lyrics = transcribe(karaoke_vocals)
-    result_speech = compare_speech(songs[song_chosen], recognized_lyrics)
-    result_bar_chart = pd.DataFrame(
-        {
-            "Kriterium": ["Gesang", "Aussprache"],
-            "Schulnote": [result_singing, result_speech],
-        })
-    result_singing = components.Textbox(value=result_singing, visible=True)
-    result_speech = components.Textbox(value=result_speech, visible=True)
-    bar_chart = components.BarPlot(value=result_bar_chart, x="Kriterium", y="Schulnote", x_title="Kriterium",
-                                   y_title="Schulnote", tooltip=["Kriterium", "Schulnote"], vertical=False,
-                                   visible=True)
+    result_speech_percentage = compare_speech(songs[song_chosen], recognized_lyrics)
+    fig = plot_results(result_singing_percentage, result_speech_percentage)
+
     result_recognized_lyrics = components.Textbox(value=recognized_lyrics, visible=True)
-    avg_semitones_difference, b, c, d = compare_avg_notes(karaoke_vocals, songs[song_chosen].song_path)
-    result_avg_semitones = components.Textbox(value=avg_semitones_difference, visible=True)
-    return {singing_layout: gr.Column(visible=False),
-            result_singing_out: result_singing,
-            result_speech_out: result_speech,
-            result_bar_chart_out: bar_chart,
+    # Compare the average notes between the two audio files
+    overall_semitone_difference, semitone_differences, avg_notes_original, avg_notes_cover = compare_avg_notes(
+        karaoke_vocals, songs[song_chosen].vocal_path)
+    # Automatically determine transposition
+    transposition = auto_transpose_avg_notes(avg_notes_original, avg_notes_cover)
+    # Transpose the cover notes
+    transposed_cover_notes = transpose_notes(avg_notes_cover, -transposition)
+    # Calculate the average differences in semitones for the transposed cover notes
+    overall_transposed_semitone_difference, transposed_semitone_differences = calculate_diff(avg_notes_original,
+                                                                                             transposed_cover_notes)
+    if transposition == 1:
+        result_transposition = components.Textbox(
+            value=f"Das Lied wurde um {transposition} Halbtonschritt transponiert.\n"
+                  f"Abweichung in Halbtonschritten: {overall_transposed_semitone_difference}",
+            visible=True)
+    else:
+        result_transposition = components.Textbox(
+            value=f"Das Lied wurde um {transposition} Halbtonschritte transponiert.\n"
+                  f"Abweichung in Halbtonschritten: {overall_transposed_semitone_difference}",
+            visible=True)
+    return {result_layout: gr.Column(visible=True),
+            singing_layout: gr.Column(visible=False),
+            result_plot: components.Plot(value=fig, visible=True),
             result_recognized_lyrics_out: result_recognized_lyrics,
-            result_avg_semitones_out: result_avg_semitones,
+            result_avg_semitones_out: result_transposition,
+            restart_btn: components.Button(visible=True),
             error_txt: components.Textbox(visible=False)
+            }
+
+
+def restart():
+    global finished_recording
+    global paused
+    global aborted_recording
+
+    finished_recording = False
+    paused = False
+    aborted_recording = False
+
+    return {result_layout: gr.Column(visible=False),
+            starting_layout: gr.Row(visible=True),
+            song_chosen_inp: components.Dropdown(value=None, label="Wähle ein Lied aus", visible=True),
+            recording_animation_video: components.Video(value=None)
             }
 
 
@@ -158,45 +212,48 @@ with gr.Blocks() as demo:
         # KaraokeNet
         Wähle ein Lied aus und gebe dein bestes es so gut wie möglich nachzusingen.
         """)
-    error_txt = gr.Textbox(label="Error", value="Du musst zuerst ein Lied auswählen", visible=False)
+    error_txt = components.Textbox(label="Error", value="Du musst zuerst ein Lied auswählen", visible=False)
     with gr.Row() as starting_layout:
-        song_chosen_inp = gr.Dropdown(song_names, label="Wähle ein Lied aus")
+        song_chosen_inp = components.Dropdown(song_names, label="Wähle ein Lied aus")
         with gr.Column():
-            train_song_btn = gr.Button("Lerne und übe das Lied")
-            ready_to_begin_btn = gr.Button("Klicke hier zum Starten")
+            train_song_btn = components.Button("Lerne und übe das Lied")
+            ready_to_begin_btn = components.Button("Klicke hier zum Starten")
 
     with gr.Column() as testing_layout:
-        song_out = gr.Audio(label="Song", visible=False)
-        instrumental_out = gr.Audio(label="Instrumentalversion", visible=False)
+        song_out = components.Audio(label="Song", visible=False)
+        instrumental_out = components.Audio(label="Instrumentalversion (KI-generiert)", visible=False)
         lyrics_out = components.Textbox(visible=False)
 
     with gr.Column() as singing_layout:
-        recording_animation_video = gr.PlayableVideo(visible=False, interactive=False)
-        get_result_btn = gr.Button(value="Klicke mich um das Ergebnis auszuwerten", visible=False)
+        recording_animation_video = components.Video(visible=False, interactive=False)
+        get_result_btn = components.Button(value="Klicke mich um das Ergebnis zu erhalten", visible=False)
 
-    with gr.Row() as result_layout:
-        with gr.Column():
-            result_singing_out = components.Textbox(label="Gesang", visible=False)
-            result_speech_out = components.Textbox(label="Aussprache", visible=False)
-        result_bar_chart_out = components.BarPlot(label="Bewertung", visible=False)
-    result_recognized_lyrics_out = components.Textbox(label="KI-generierte Lyrics deiner Aufnahme", visible=False)
-    result_avg_semitones_out = components.Textbox(label="Durchschnittler Unterschied in Seminoten", visible=False)
+    with gr.Column() as result_layout:
+        result_plot = components.Plot(label="Bewertung", visible=False)
+        result_avg_semitones_out = components.Textbox(label="Detaillierte Analyse des Gesangs", visible=False)
+        result_recognized_lyrics_out = components.Textbox(label="KI-generierte Lyrics deiner Aufnahme", visible=False)
+        restart_btn = components.Button(value="Hier gelangst du zum Startbildschirm zurück", visible=False)
 
-    train_song_btn.click(fn=train_song, inputs=[song_chosen_inp], outputs=[song_out, instrumental_out, lyrics_out,
-                                                                           error_txt])
-    ready_to_begin_btn.click(fn=change_layout, inputs=[song_chosen_inp], outputs=[starting_layout, testing_layout,
+
+    train_song_btn.click(fn=train_song, inputs=[song_chosen_inp], outputs=[testing_layout, song_out, instrumental_out,
+                                                                           lyrics_out, error_txt])
+    ready_to_begin_btn.click(fn=change_layout, inputs=[song_chosen_inp], outputs=[singing_layout, starting_layout,
+                                                                                  testing_layout,
                                                                                   recording_animation_video,
                                                                                   get_result_btn, error_txt])
 
     recording_animation_video.play(fn=start, inputs=[song_chosen_inp], outputs=[])
     recording_animation_video.pause(fn=pause, inputs=[], outputs=[])
 
-    get_result_btn.click(fn=estimate_result, inputs=[song_chosen_inp], outputs=[singing_layout,
-                                                                                result_singing_out,
-                                                                                result_speech_out, result_bar_chart_out,
+    get_result_btn.click(fn=estimate_result, inputs=[song_chosen_inp], outputs=[result_layout,
+                                                                                singing_layout,
+                                                                                result_plot,
                                                                                 result_recognized_lyrics_out,
                                                                                 result_avg_semitones_out,
+                                                                                restart_btn,
                                                                                 error_txt])
+    restart_btn.click(fn=restart, inputs=[], outputs=[result_layout, starting_layout, song_chosen_inp,
+                                                      recording_animation_video])
 
 if __name__ == "__main__":
     demo.launch()
