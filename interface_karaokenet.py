52efbddcc003e19af36f3445a33690b0eb6fbd24
Eduard Gashi <165167290+eduardgashi24@users.noreply.github.com>
2024-06-30 18:50:59 +0200

    Update interface_karaokenet.py

--- interface_karaokenet.py
+++ interface_karaokenet.py
@@ -3,13 +3,10 @@ from gradio import components
 from song import songs
 import wave
 import pyaudio
-from . import mfcc_comparison
-from . import speech_comparison
-from mfcc_comparison import compare_mfcc
-from speech_comparison import compare_speech
-import pandas as pd
-from speech_comparison import transcribe
-from pitch_comparison_transposition import compare_avg_notes
+import matplotlib.pyplot as plt
+from speech_comparison import compare_speech, transcribe
+from pitch_comparison_transposition import (auto_transpose_avg_notes, transpose_notes, calculate_diff, calculate_grade,
+                                            compare_current_notes)
 import Sender
 import compareClass
 import socket
@@ -19,7 +16,7 @@ import pickle
 
 
 # Ordner, in dem die Karaoke-Version des Benutzers gespeichert wird
-vocals_user_dir = r"C:/Users/20edu/Softwareprojekt/pythonProject1/vocals_user/[Karaoke] "
+vocals_user_dir = r"vocals_user/[Karaoke] "
 song_names = list(songs.keys())  # Liste, in der die Namen aller Lieder sind
 
 # Globale Variablen, die wichtig sind, wenn das Video abgebrochen wird (ungültige Karaoke-Version)
@@ -43,6 +40,8 @@ def receive():
         raise RuntimeError(e)
 
 
+# Erstellt eine Audioaufnahme des Benutzers während er singt und speichert diese mit dem Namen
+# [Karaoke] Name_des_Liedes.wav
 def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, format=pyaudio.paInt16, channels=1,
                  rate=44100):
     global paused
@@ -67,7 +66,7 @@ def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, fo
             aborted_recording = True
             break
 
-    # Datenfluss bzw. Aufnahme stoppen
+    # Datenfluss bzw. Speichern der Frames stoppen
     stream.stop_stream()
     stream.close()
     p.terminate()
@@ -80,32 +79,36 @@ def record_audio(length_in_seconds, output_file_name, frames_per_buffer=1024, fo
         obj.setframerate(rate)
         obj.writeframes(b''.join(frames))
         obj.close()
-        finished_recording = True
+        finished_recording = True  # Hiernach kann der Benutzer ein Ergebnis erhalten
 
 
 def train_song(song_chosen):
-    if song_chosen is None:
-        return {error_txt: components.Textbox(visible=True)}
-    song_audio = gr.Audio(value=songs[song_chosen].song_path, visible=True)
-    instrumental_audio = gr.Audio(value=songs[song_chosen].instrumental_path, visible=True)
+    if song_chosen is None:  # Fehler, wenn der Benutzer kein Lied ausgewählt hat
+        return {song_chosen_error: components.Textbox(visible=True)}
+    song = components.Audio(value=songs[song_chosen].song_path, visible=True)
+    instrumental = components.Audio(value=songs[song_chosen].instrumental_path, visible=True)
     lyrics = components.Textbox(label=song_chosen, value=songs[song_chosen].lyrics, visible=True)
-    return {testing_layout: gr.Column(visible=True), song_out: song_audio, instrumental_out: instrumental_audio,
-            lyrics_out: lyrics, error_txt: gr.Textbox(visible=False)}
+    return {testing_layout: gr.Column(visible=True), song_out: song, instrumental_out: instrumental,
+            lyrics_out: lyrics, song_chosen_error: gr.Textbox(visible=False)}
 
 
+# Wechselt das Layout zum Layout, in dem gesungen wird
 def change_layout(song_chosen):
-    if song_chosen is None:
-        return {error_txt: components.Textbox(visible=True)}
-    animation_video = components.Video(value=songs[song_chosen].animation_path, visible=True)
+    if song_chosen is None:  # Fehler, wenn der Benutzer kein Lied ausgewählt hat
+        return {song_chosen_error: components.Textbox(visible=True)}
+    # Initialisiert das Video mit dem zum Lied gehörigen Video
+    video = components.Video(value=songs[song_chosen].video_path, visible=True)
     return {singing_layout: gr.Column(visible=True),
             starting_layout: gr.Row.update(visible=False), testing_layout: gr.Column(visible=False),
-            recording_animation_video: animation_video,
-            get_result_btn: components.Button(visible=True), error_txt: components.Textbox(visible=False)}
+            recording_lyrics_video_out: video, get_result_btn: components.Button(visible=True),
+            song_chosen_error: components.Textbox(visible=False)}
 
 
+# Wird aufgerufen, wenn der Benutzer das Video abspielt und damit eine Aufnahme startet
 def start(song_chosen):
     global paused
-    paused = False  # reset auf False
+    paused = False  # Reset auf False
+    # Starten einer Aufnahme mit der Länge des Liedes und speichern dieser mit dem entsprechenden Namen
     record_audio(songs[song_chosen].length, songs[song_chosen].name)
     return {}
 
@@ -120,21 +123,21 @@ def rgb_to_hex(rgb):
 
 
 def plot_results(result_dict):
-    labels = list(result_dict.keys())
-    values = list(result_dict.values())
+    labels = list(result_dict.keys())  # In den labels sind die einzelnen Kriterien
+    values = list(result_dict.values())  # Die entsprechenden Werte zu den einzelnen Kriterien
 
     fig, ax = plt.subplots(figsize=(13, 3), dpi=300)
 
     bars_color_hex = rgb_to_hex((37, 150, 190))
-    ax.barh(labels, values, color=bars_color_hex)
-    ax.set_xlim(0, 100)
-    ax.set_xticks([0, 50, 100])  # nur bei dem Wert 0, 50 und 100 werden die labels angezeigt
+    ax.barh(labels, values, color=bars_color_hex)  # Ergebnis-Balken mit hellblauer Farbe
+    ax.set_xlim(0, 100)  # Werte zwischen 0 und 100, die einzelnen Prozentzahlen als Ergebnis
+    ax.set_xticks([0, 50, 100])  # Nur bei dem Wert 0, 50 und 100 werden die labels angezeigt
 
-    background_color_hex = rgb_to_hex((34, 41, 54))
+    background_color_hex = rgb_to_hex((34, 41, 54))  # Dunkleres blau als Hintergrund
     ax.set_xticklabels(["Ausbaufähig", "Befriedigend", "Sehr gut"])
     ax.set_facecolor(background_color_hex)
     fig.patch.set_facecolor(background_color_hex)
-    ax.tick_params(axis='x', colors="white")
+    ax.tick_params(axis='x', colors="white")  # Alle Umrandungen in weißer Farbe
     ax.tick_params(axis='y', colors="white")
     for spine in ax.spines.values():
         spine.set_edgecolor("white")
@@ -144,9 +147,9 @@ def plot_results(result_dict):
 
 def estimate_result(song_chosen):
     global finished_recording
-    if not finished_recording:
-        return {error_txt2: components.Textbox(visible=True)}
-    user_vocals = vocals_user_dir + song_chosen + ".wav"
+    if not finished_recording:  # Bei ungültiger Version erhält der Nutzer kein Ergebnis
+        return {cancel_error: components.Textbox(visible=True)}
+    user_vocals = vocals_user_dir + song_chosen + ".wav"  # Pfad zur gerade erstellten Aufnahme des Benutzers
     # save wav file as object to transfer to server
     # karaoke_wav = wave.open(karaoke_vocals)
     # song_wav = wave.open(songs[song_chosen].song_path)
@@ -171,14 +174,18 @@ def estimate_result(song_chosen):
     # Calculate the percentage score based on the transposed average difference in semitones
     result_singing_percentage = calculate_grade(overall_semitone_difference)
 
+    # Erstellt den KI-generierten Text der Aufnahme des Benutzers
     recognized_lyrics = transcribe(user_vocals)
     result_speech_percentage = compare_speech(songs[song_chosen], recognized_lyrics)
     result_rhythm_percentage = 70
+    # Dictionary, in dem die Kriterien die keys und die dazugehörigen Ergebnisse die values sind
     result_dict = {"Aussprache": result_speech_percentage, "Rhythmik": result_rhythm_percentage,
                    "Gesang": result_singing_percentage}
-    fig = plot_results(result_dict)
 
-    result_recognized_lyrics = components.Textbox(value=recognized_lyrics, visible=True)
+    fig = plot_results(result_dict)  # Erstellt das Balkendiagramm
+
+    # Textbox, in der die KI-generierten Lyrics des Benutzers angezeigt werden
+    result_recognized_lyrics = gr.Textbox(value=recognized_lyrics, visible=True)
 
     if transposition == 1:
         result_transposition = components.Textbox(
@@ -192,11 +199,11 @@ def estimate_result(song_chosen):
             visible=True)
     return {result_layout: gr.Column(visible=True),
             singing_layout: gr.Column(visible=False),
-            result_plot: components.Plot(value=fig, visible=True),
+            result_plot_out: components.Plot(value=fig, visible=True),
             result_recognized_lyrics_out: result_recognized_lyrics,
-            result_avg_semitones_out: result_transposition,
+            result_singing_detailed_out: result_transposition,
             restart_btn: components.Button(visible=True),
-            error_txt2: components.Textbox(visible=False)
+            cancel_error: components.Textbox(visible=False)
             }
 
 
@@ -212,60 +219,84 @@ def restart():
     return {result_layout: gr.Column(visible=False),
             starting_layout: gr.Row(visible=True),
             song_chosen_inp: components.Dropdown(value=None, label="Wähle ein Lied aus", visible=True),
-            recording_animation_video: components.Video(value=None)
+            recording_lyrics_video_out: components.Video(value=None)
             }
 
 
 with gr.Blocks() as demo:
+    # Titel des Projekts mit Untertitel
     gr.Markdown(
         """
         # KaraokeNet
         Bringe deine Karaoke Skills auf ein neues Level.
         """)
-    error_txt = components.Textbox(label="Error", value="Du musst zuerst ein Lied auswählen", visible=False)
+
+    # Fehleranzeige, falls kein Lied ausgewählt ist
+    song_chosen_error = components.Textbox(label="Error", value="Du musst zuerst ein Lied auswählen", visible=False)
+
+    # starting_layout dient als Container für die Startoptionen
     with gr.Row() as starting_layout:
+        # Dropdown-Menü zur Auswahl des Songs. Nachdem der Benutzer seien Wahl getroffen hat,
+        # wird in song_chosen_inp der Name des ausgewählten Liedes gespeichert.
         song_chosen_inp = components.Dropdown(song_names, label="Wähle ein Lied aus")
         with gr.Column():
+            # Button zum Üben des ausgewählten Songs
             train_song_btn = components.Button("Lerne und übe das Lied")
+            # Button zum Starten der Aufnahme
             ready_to_begin_btn = components.Button("Klicke hier zum Starten")
 
+    # Container für die Testoptionen
     with gr.Column() as testing_layout:
+        # Audio-Ausgabe für den Song
         song_out = components.Audio(label="Song", visible=False, min_width=100, show_download_button=False)
+        # Audio-Ausgabe für die Instrumentalversion
         instrumental_out = components.Audio(label="Instrumentalversion (KI-generiert)", visible=False, min_width=100,
                                             show_download_button=False)
+        # Textbox für die Anzeige der Lyrics
         lyrics_out = components.Textbox(visible=False)
 
+    # Container für die Aufnahmeoptionen
     with gr.Column() as singing_layout:
-        recording_animation_video = components.Video(visible=False, interactive=False, show_download_button=False)
-        error_txt2 = components.Textbox(label="Error", value="Du musst zum gesamten Lied singen.", visible=False)
+        # Video-Komponente für die Aufnahmeanimation
+        recording_lyrics_video_out = components.Video(visible=False, interactive=False, show_download_button=False)
+        # Fehleranzeige, falls nicht zum gesamten Lied gesungen wird
+        cancel_error = components.Textbox(label="Error", value="Du musst zum gesamten Lied singen.", visible=False)
+        # Button zur Ergebnisauswertung
         get_result_btn = components.Button(value="Ergebnis auswerten", visible=False)
 
+    # Container für die Ergebnisanzeige
     with gr.Column() as result_layout:
-        result_plot = components.Plot(label="Bewertung", visible=False)
-        result_avg_semitones_out = components.Textbox(label="Detaillierte Analyse des Gesangs", visible=False)
+        # Balkendiagramm zur Bewertung der Karaoke-Version des Benutzers
+        result_plot_out = components.Plot(label="Bewertung", visible=False)
+        # Textbox für die detaillierte Analyse des Gesangs
+        result_singing_detailed_out = components.Textbox(label="Detaillierte Analyse des Gesangs", visible=False)
         result_recognized_lyrics_out = components.Textbox(label="KI-generierte Lyrics deiner Aufnahme", visible=False)
         restart_btn = components.Button(value="Startmenü", visible=False)
 
+    # Bei Klick auf den "Lerne und übe das Lied"-Button wird die train_song-Funktion mit den angegebenen inputs
+    # als Eingabewerten aufgerufen. Die Rückgabewerte der Funktion werden den outputs zugewiesen.
     train_song_btn.click(fn=train_song, inputs=[song_chosen_inp], outputs=[testing_layout, song_out, instrumental_out,
-                                                                           lyrics_out, error_txt])
+                                                                           lyrics_out, song_chosen_error])
+    # Bei Klick auf den "Klicke hier zum Starten"-Button wird die Aufnahme gestartet
     ready_to_begin_btn.click(fn=change_layout, inputs=[song_chosen_inp], outputs=[singing_layout, starting_layout,
                                                                                   testing_layout,
-                                                                                  recording_animation_video,
-                                                                                  get_result_btn, error_txt])
-
-    recording_animation_video.play(fn=start, inputs=[song_chosen_inp], outputs=[])
-    recording_animation_video.pause(fn=pause, inputs=[], outputs=[])
-
+                                                                                  recording_lyrics_video_out,
+                                                                                  get_result_btn, song_chosen_error])
+    # Startet die Aufnahme, wenn das Musikvideo abgespielt wird
+    recording_lyrics_video_out.play(fn=start, inputs=[song_chosen_inp], outputs=[])
+    # Pausiert die Aufnahme, wenn das Musikvideo pausiert wird
+    recording_lyrics_video_out.pause(fn=pause, inputs=[], outputs=[])
+    # Bei Klick auf den "Ergebnis auswerten"-Button wird die estimate_result-Funktion aufgerufen
     get_result_btn.click(fn=estimate_result, inputs=[song_chosen_inp], outputs=[result_layout,
                                                                                 singing_layout,
-                                                                                result_plot,
+                                                                                result_plot_out,
                                                                                 result_recognized_lyrics_out,
-                                                                                result_avg_semitones_out,
+                                                                                result_singing_detailed_out,
                                                                                 restart_btn,
-                                                                                error_txt2])
+                                                                                cancel_error])
+    # Bei Klick auf den "Startmenü"-Button wird das Layout zurückgesetzt
     restart_btn.click(fn=restart, inputs=[], outputs=[result_layout, starting_layout, song_chosen_inp,
-                                                      recording_animation_video])
+                                                      recording_lyrics_video_out])
 
 if __name__ == "__main__":
     demo.launch()
-
